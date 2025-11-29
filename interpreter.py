90915f81438efb71f62ad8e058ca6b22860fae12
reniejoshi <renie_joshi@yahoo.com>
2025-11-28 16:50:07 -0800

    Add methods to initialize, set, and get variables in Variables class

--- interpreter.py
+++ interpreter.py
@@ -1,5 +1,13 @@
 class Variables:
-    pass
+    def __init__(self):
+        # Dictionary to store variables
+        self.variables = {}
+    
+    def set_variable(self, name, value):
+        self.variables[name] = value
+    
+    def get_variable(self, name):
+        return self.variables[name]
 
 class Interpreter:
     pass
