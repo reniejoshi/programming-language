3824e77bc80d6abbfa8ed7e80ef1d415f9f0b74e
reniejoshi <renie_joshi@yahoo.com>
2025-11-29 12:09:29 -0800

    Refactor Tokenizer
    Add helper methods to handle indexing, rename variables for clarity, and make is_int() and is_float() instance methods.

--- tokenizer.py
+++ tokenizer.py
@@ -3,9 +3,26 @@ class Tokenizer:
     # Method to initialize object of class
     def __init__(self, code_string):
         self.code_string = code_string
+        # Index in code_string
+        self.index = 0
+    
+    def current_char(self):
+        if self.index < len(self.code_string):
+            return self.code_string[self.index]
+        else:
+            return "EOF"
+    
+    def next_char(self):
+        if self.index + 1 < len(self.code_string):
+            return self.code_string[self.index + 1]
+        else:
+            return "EOF"
+
+    # Method that increments index
+    def consume(self, increment_value = 1):
+        self.index += increment_value
 
     def tokenize(self):
-        code_string = self.code_string
         # List of tuples holding type and value
         self.tokens = []
         # Dictionary of keywords
@@ -19,105 +36,101 @@ class Tokenizer:
         # List of comparision operators
         comparision_operators = ["==", "!=", ">", "<", ">=", "<="]
 
-        # Index in code_string
-        self.i = 0
-
         # Loop to iterate through code_string
-        while self.i < len(code_string):
-            char = code_string[self.i]
-
-            # If char is space, continue
-            if char.isspace():
-                self.i += 1
+        while self.current_char() != "EOF":
+            # If current char is space, continue
+            if self.current_char().isspace():
+                self.consume()
                 continue
 
-            # If char is the start of an identifier in camel case or keyword, append the identifer or keyword to tokens
-            elif char.isalpha():
-                start_index = self.i
-                self.i += 1
-                while self.i < len(code_string) and code_string[self.i].isalpha():
-                    self.i += 1
-                if code_string[start_index:self.i] in keywords:
-                    token_type = code_string[start_index:self.i]
-                    self.tokens.append((keywords[token_type], code_string[start_index:self.i]))
+            # If current char is the start of an identifier in camel case or keyword, append the identifer or keyword to tokens
+            elif self.current_char().isalpha():
+                start_index = self.index
+                self.consume()
+                while self.current_char() != "EOF" and self.current_char().isalpha():
+                    self.consume()
+                if self.code_string[start_index:self.index] in keywords:
+                    keyword = self.code_string[start_index:self.index]
+                    self.tokens.append((keywords[keyword], keyword))
                 else:
-                    self.tokens.append(("IDENTIFIER", code_string[start_index:self.i]))
+                    self.tokens.append(("IDENTIFIER", self.code_string[start_index:self.index]))
                 continue
             
-            # If char is a number, append the number to tokens
-            elif char.isnumeric():
-                start_index = self.i
-                self.i += 1
-                while self.i < len(code_string) and (code_string[self.i].isnumeric() or code_string[self.i] == "."):
-                    self.i += 1
-                number = code_string[start_index:self.i]
-                if Tokenizer.is_int(number):
+            # If current char is a number, append the number to tokens
+            elif self.current_char().isnumeric():
+                start_index = self.index
+                self.consume()
+                while self.current_char() != "EOF" and (self.current_char().isnumeric() or self.current_char() == "."):
+                    self.consume()
+                number = self.code_string[start_index:self.index]
+                if self.is_int(number):
                     self.tokens.append(("INTEGER", int(number)))
-                elif Tokenizer.is_float(number):
+                elif self.is_float(number):
                     self.tokens.append(("FLOAT", float(number)))
                 continue
 
-            # If char is start of a string, append the string to tokens
-            elif char == "\"" or char == "'":
-                self.i += 1
-                start_index = self.i
-                while self.i < len(code_string) and code_string[self.i] != "\"" and code_string[self.i] != "'":
-                    self.i += 1
-                self.tokens.append(("STRING", code_string[start_index:self.i]))
-                self.i += 1
+            # If current char is start of a string, append the string to tokens
+            elif self.current_char() == "\"" or self.current_char() == "'":
+                self.consume()
+                start_index = self.index
+                while self.current_char() != "EOF" and self.current_char() != "\"" and self.current_char() != "'":
+                    self.consume()
+                self.tokens.append(("STRING", self.code_string[start_index:self.index]))
+                self.consume()
                 continue
 
-            # If char is variable assignment, append the assignment symbol to tokens
-            elif char == "=" and code_string[self.i + 1] != "=":
-                self.tokens.append(("ASSIGNMENT", code_string[self.i]))
-                self.i += 1
+            # If current char is variable assignment, append the assignment symbol to tokens
+            elif self.current_char() == "=" and self.next_char() != "=":
+                self.tokens.append(("ASSIGNMENT", self.current_char()))
+                self.consume()
                 continue
 
-            # If char is an arithmetic operator, append the arithmetic operator to tokens
-            elif char in arithmetic_operators:
-                self.tokens.append(("ARITHMETIC_OPERATOR", code_string[self.i]))
-                self.i += 1
+            # If current char is an arithmetic operator, append the arithmetic operator to tokens
+            elif self.current_char() in arithmetic_operators:
+                self.tokens.append(("ARITHMETIC_OPERATOR", self.current_char()))
+                self.consume()
                 continue
 
-            # If char is the start of a comparision operator, append the comparision operator to tokens
-            elif char + code_string[self.i] in comparision_operators:
-                self.tokens.append(("COMPARISION_OPERATOR", char + code_string[self.i]))
-                self.i += 2
+            # If current char is the start of a comparision operator, append the comparision operator to tokens
+            elif self.current_char() + self.next_char() in comparision_operators:
+                self.tokens.append(("COMPARISION_OPERATOR", self.current_char() + self.next_char()))
+                self.consume(2)
                 continue
 
-            # If char is open brace, append open brace to tokens
-            elif char == "{":
-                self.tokens.append(("OPEN_BRACE", code_string[self.i]))
-                self.i += 1
+            # If current char is open brace, append open brace to tokens
+            elif self.current_char() == "{":
+                self.tokens.append(("OPEN_BRACE", self.current_char()))
+                self.consume()
                 continue
             
-            # If char is close brace, append close brace to tokens
-            elif char == "}":
-                self.tokens.append(("CLOSE_BRACE", code_string[self.i]))
-                self.i += 1
+            # If current char is close brace, append close brace to tokens
+            elif self.current_char() == "}":
+                self.tokens.append(("CLOSE_BRACE", self.current_char()))
+                self.consume()
                 continue
 
-            # If char is newline, append new line to tokens
-            elif char == "\n":
-                self.tokens.append(("NEWLINE", code_string[self.i]))
-                self.i += 1
+            # If current char is newline, append new line to tokens
+            elif self.current_char() == "\n":
+                self.tokens.append(("NEWLINE", self.current_char()))
+                self.consume()
                 continue
 
-            # Increment i
-            self.i += 1
+            # Increment index
+            else:
+                self.consume()
             
         return self.tokens
     
-    def is_int(num):
+    def is_int(self, number):
         try:
-            int(num)
+            int(number)
             return True
         except ValueError:
             return False
 
-    def is_float(num):
+    def is_float(self, number):
         try:
-            float(num)
+            float(number)
             return True
         except ValueError:
             return False
